20c57659c60ca16c7607549267f56e4fbc8bbb87
jorge bejarano <jorge.bejarano@yahoo.com>
2020-06-12 10:19:07 -0400

    Adding methods to customlist class

--- CustomList/CustomList.cs
+++ CustomList/CustomList.cs
@@ -8,6 +8,37 @@ namespace CustomList
 {
     public class MyList<T>
     {
-       
+        private T[] items;
+        int count;
+        public int Count
+        {
+            get
+            {
+                return this.count;
+            }
+            set
+            {
+                this.count = value;
+            }
+        }
+        int capacity;
+        public int Capacity
+        {
+            get
+            {
+                return this.capacity;
+            }
+            set
+            {
+                this.capacity = value;
+            }
+        }
+
+        public MyList()
+        {
+            Count = 0;
+            Capacity = 4;
+            items = new T[Capacity];
+        }
     }
 }
